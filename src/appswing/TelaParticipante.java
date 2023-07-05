0e0e2d79a947e7578161b107c158de83e6df8db8
nonluck <luiskilmerluk1221@gmail.com>
2023-07-05 10:47:36 -0300

    the end...

--- src/appswing/TelaParticipante.java
+++ src/appswing/TelaParticipante.java
@@ -13,7 +13,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.lang.reflect.Executable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
